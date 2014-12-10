9bfa35a1f2135aa4b09d88e8c593370879b8eee3
Julien DIDIER <julien@didier.io>
2014-12-10 15:21:18 +0100

    option lang

--- test.php
+++ test.php
@@ -6,10 +6,10 @@ use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 require_once __DIR__.'/vendor/autoload.php';
-
 
 class HelloCommand extends Command
 {
@@ -18,12 +18,22 @@ class HelloCommand extends Command
         $this
             ->setName('hello')
             ->addArgument('username', InputArgument::OPTIONAL, 'Who do you want to say hello?', 'world')
+            ->addOption('lang', null, InputOption::VALUE_REQUIRED, 'What language would you want to say hello?', 'en')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln(sprintf('Hello %s!', $input->getArgument('username')));
+        $patterns = [
+            'en' => 'Hello %s!',
+            'fr' => 'Bonjour %s !',
+            'es' => '¡ Hola %s !',
+        ];
+
+        $pattern = $patterns[$input->getOption('lang')];
+        $username = $input->getArgument('username');
+
+        $output->writeln(sprintf($pattern, $username));
     }
 }
 
